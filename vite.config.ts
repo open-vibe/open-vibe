1a97d05f6e148cb4dae6e014d143a1dfb65b69bf
citizenl <hi@citizenl.me>
2026-01-29 08:23:07 +0800

    feat:vite

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,8 @@ import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 
 // @ts-expect-error process is a nodejs global
-const host = process.env.TAURI_DEV_HOST;
+const devHost = process.env.TAURI_DEV_HOST;
+const host = devHost || "127.0.0.1";
 
 const packageJson = JSON.parse(
   readFileSync(new URL("./package.json", import.meta.url), "utf-8"),
@@ -34,11 +35,11 @@ export default defineConfig(async () => ({
   server: {
     port: 1420,
     strictPort: true,
-    host: host || false,
-    hmr: host
+    host,
+    hmr: devHost
       ? {
           protocol: "ws",
-          host,
+          host: devHost,
           port: 1421,
         }
       : undefined,
